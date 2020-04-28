92171ff3647a46c30cc0e422e6850159f55f7788
David Foster <dlfosterii@yahoo.com>
2020-04-28 15:13:01 -0400

    complete

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ dogButton.addEventListener('click', function () {
     dogButton.textContent = 'Generate Doggo';
   });
 });
+
+window.addEventListener('DOMContentLoaded', function() {
+    $.get('https://dog.ceo/api/breeds/list')
+        .then (function(data) {
+            const breeds = data.message;
+            const select = document.getElementById('dropdown')
+            breeds.forEach(function(breed){
+                const option = document.createElement('option');
+                option.setAttribute('value', breed);
+                option.textContent = breed;
+                select.appendChild(option);
+        })
+    })
+
+})
+
+let select = document.getElementById('dropdown')
+select.addEventListener('change', function(event) {
+    let breed = event.target.value;
+    $.get(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then (function (data){
+        const img = document.createElement('img');
+        img.setAttribute('src', data.message);
+        const imagesDiv = document.getElementById('images');
+        imagesDiv.innerHTML = '';
+        imagesDiv.appendChild(img);
+    })
+
+})
